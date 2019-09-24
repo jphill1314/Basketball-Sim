77f2994695407c2429726a7fb20affc8225fa758
jphill1314 <jphill1314@gmail.com>
2019-09-24 19:21:45 -0400

    more view binding conversions

--- app/src/main/java/com/appdev/jphil/basketballcoach/roster/RosterAdapter.kt
+++ app/src/main/java/com/appdev/jphil/basketballcoach/roster/RosterAdapter.kt
@@ -6,8 +6,9 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.appdev.jphil.basketballcoach.R
+import com.appdev.jphil.basketballcoach.databinding.ListItemHeaderBinding
+import com.appdev.jphil.basketballcoach.databinding.ListItemRosterBinding
 
 class RosterAdapter(
     val presenter: RosterContract.Presenter,
@@ -19,21 +20,16 @@ class RosterAdapter(
     private val classes: Array<String> = resources.getStringArray(R.array.years)
     var isUsersTeam = false
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val name: TextView? = view.findViewById(R.id.name)
-        val rating: TextView? = view.findViewById(R.id.rating)
-        val position: TextView? = view.findViewById(R.id.position)
-        val year: TextView? = view.findViewById(R.id.year)
-        val title: TextView? = view.findViewById(R.id.title)
-    }
+    abstract class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+    class PlayerViewHolder(val binding: ListItemRosterBinding): ViewHolder(binding.root)
+    class HeaderViewHolder(val binding: ListItemHeaderBinding): ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, type: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
         return if (type == 0) {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_header, parent, false)
-            ViewHolder(view)
+            HeaderViewHolder(ListItemHeaderBinding.inflate(inflater, parent, false))
         } else {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_roster, parent, false)
-            ViewHolder(view)
+            PlayerViewHolder(ListItemRosterBinding.inflate(inflater, parent, false))
         }
     }
 
@@ -50,59 +46,69 @@ class RosterAdapter(
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        if (position == 0 || position == 7) {
-            if (position == 0) {
-                viewHolder.title?.text = resources.getString(R.string.starting_lineup)
-            } else {
-                viewHolder.title?.text = resources.getString(R.string.bench)
-            }
-        } else if (position == 1 || position == 8) {
-            viewHolder.position?.text = resources.getString(R.string.pos)
-            viewHolder.name?.text = resources.getString(R.string.name)
-            viewHolder.rating?.text = resources.getString(R.string.rating)
-            viewHolder.year?.text = resources.getString(R.string.year)
+        when (viewHolder) {
+            is HeaderViewHolder -> bindHeader(viewHolder.binding, position)
+            is PlayerViewHolder -> bindPlayer(viewHolder.binding, position)
+        }
+    }
 
-            val params = viewHolder.itemView.layoutParams as ViewGroup.MarginLayoutParams
+    private fun bindHeader(binding: ListItemHeaderBinding, position: Int) {
+        if (position == 0) {
+            binding.title.text = resources.getString(R.string.starting_lineup)
+        } else {
+            binding.title.text = resources.getString(R.string.bench)
+        }
+    }
+
+    private fun bindPlayer(binding: ListItemRosterBinding, position: Int) {
+        if (position == 1 || position == 8) {
+            binding.position?.text = resources.getString(R.string.pos)
+            binding.name?.text = resources.getString(R.string.name)
+            binding.rating?.text = resources.getString(R.string.rating)
+            binding.year?.text = resources.getString(R.string.year)
+
+            val params = binding.root.layoutParams as ViewGroup.MarginLayoutParams
             params.bottomMargin = resources.getDimensionPixelSize(R.dimen.card_margin_bottom)
-            setTextColor(viewHolder, false)
+            setTextColor(binding, false)
         } else {
             val dataModel = if (position < 7) roster[position - 2] else roster[position - 4]
             val player = dataModel.player
-            setTextColor(viewHolder, dataModel.isSelected)
+            setTextColor(binding, dataModel.isSelected)
 
-            viewHolder.position?.text = if (position < 7) positions[position - 2] else positions[player.position - 1]
-            viewHolder.name?.text = player.fullName
-            viewHolder.rating?.text = player.getOverallRating().toString()
-            viewHolder.year?.text = classes[player.year]
+            binding.position?.text = if (position < 7) positions[position - 2] else positions[player.position - 1]
+            binding.name?.text = player.fullName
+            binding.rating?.text = player.getOverallRating().toString()
+            binding.year?.text = classes[player.year]
 
             if (position == 6) {
-                val params = viewHolder.itemView.layoutParams as ViewGroup.MarginLayoutParams
+                val params = binding.root.layoutParams as ViewGroup.MarginLayoutParams
                 params.bottomMargin = resources.getDimensionPixelSize(R.dimen.sixteen_dp)
             }
 
             if (isUsersTeam) {
-                viewHolder.itemView.setOnClickListener {
+                binding.root.setOnClickListener {
                     presenter.onPlayerSelected(player)
                     dataModel.isSelected = !dataModel.isSelected
                     notifyDataSetChanged()
                 }
             }
-            viewHolder.itemView.setOnLongClickListener {
+            binding.root.setOnLongClickListener {
                 presenter.onPlayerLongPressed(player)
                 true
             }
         }
     }
 
-    private fun setTextColor(viewHolder: ViewHolder, isSelected: Boolean) {
+    private fun setTextColor(binding: ListItemRosterBinding, isSelected: Boolean) {
         val color = if (isSelected) R.color.gray else R.color.black
-        viewHolder.position?.setTextColor(ResourcesCompat.getColor(resources, color, null))
-        viewHolder.name?.setTextColor(ResourcesCompat.getColor(resources, color, null))
-        viewHolder.rating?.setTextColor(ResourcesCompat.getColor(resources, color, null))
-        viewHolder.year?.setTextColor(ResourcesCompat.getColor(resources, color, null))
+        binding.position.setTextColor(ResourcesCompat.getColor(resources, color, null))
+        binding.name.setTextColor(ResourcesCompat.getColor(resources, color, null))
+        binding.rating.setTextColor(ResourcesCompat.getColor(resources, color, null))
+        binding.year.setTextColor(ResourcesCompat.getColor(resources, color, null))
     }
 
     fun updateRoster(roster: MutableList<RosterDataModel>) {
+        // TODO: diff util?
         this.roster.clear()
         this.roster.addAll(roster)
         notifyDataSetChanged()
--- app/src/main/java/com/appdev/jphil/basketballcoach/schedule/ScheduleAdapter.kt
+++ app/src/main/java/com/appdev/jphil/basketballcoach/schedule/ScheduleAdapter.kt
@@ -5,10 +5,9 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import com.appdev.jphil.basketball.datamodels.ScheduleDataModel
 import com.appdev.jphil.basketballcoach.R
+import com.appdev.jphil.basketballcoach.databinding.ListItemScheduleBinding
 
 class ScheduleAdapter(
     private val resources: Resources,
@@ -18,19 +17,10 @@ class ScheduleAdapter(
 
     var games: List<ScheduleDataModel>? = null
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val homeName: TextView = view.findViewById(R.id.home_name)
-        val awayName: TextView = view.findViewById(R.id.away_name)
-        val homeScore: TextView = view.findViewById(R.id.home_score)
-        val awayScore: TextView = view.findViewById(R.id.away_score)
-        val gameStatus: TextView = view.findViewById(R.id.game_status)
-        val simToGame: Button = view.findViewById(R.id.sim_to_game)
-        val simGame: Button = view.findViewById(R.id.sim_game)
-    }
+    class ViewHolder(val binding: ListItemScheduleBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, type: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_schedule, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(ListItemScheduleBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
     override fun getItemCount(): Int {
@@ -39,51 +29,57 @@ class ScheduleAdapter(
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         if (position < games?.size ?: 0) {
-            games?.let { handleGame(it[position], position, viewHolder) }
+            games?.let { handleGame(it[position], position, viewHolder.binding) }
         } else {
-            viewHolder.homeName.text = "Conference tournament!"
-            viewHolder.awayName.text = ""
-            viewHolder.homeScore.text = ""
-            viewHolder.awayScore.text = ""
-            viewHolder.gameStatus.text = ""
-            viewHolder.itemView.setOnClickListener { presenter.goToConferenceTournament() }
+            viewHolder.binding.apply {
+                homeName.text = "Conference tournament!"
+                awayName.text = ""
+                homeScore.text = ""
+                awayScore.text = ""
+                gameStatus.text = ""
+                root.setOnClickListener { presenter.goToConferenceTournament() }
+            }
         }
     }
 
-    private fun handleGame(game: ScheduleDataModel, position: Int, viewHolder: ViewHolder) {
-        viewHolder.homeName.text = game.homeTeamName
-        viewHolder.awayName.text = game.awayTeamName
-        if (game.isFinal) {
-            viewHolder.homeScore.text = game.homeTeamScore.toString()
-            viewHolder.awayScore.text = game.awayTeamScore.toString()
-            viewHolder.gameStatus.text = resources.getString(R.string.game_final)
-        } else if (game.inProgress){
-            viewHolder.homeScore.text = game.homeTeamScore.toString()
-            viewHolder.awayScore.text = game.awayTeamScore.toString()
-            viewHolder.gameStatus.text = resources.getString(R.string.in_progress)
-        } else {
-            viewHolder.homeScore.text = game.homeTeamRecord
-            viewHolder.awayScore.text = game.awayTeamRecord
-            viewHolder.gameStatus.text = resources.getString(R.string.game_number, position + 1)
+    private fun handleGame(game: ScheduleDataModel, position: Int, binding: ListItemScheduleBinding) {
+        binding.homeName.text = game.homeTeamName
+        binding.awayName.text = game.awayTeamName
+        when {
+            game.isFinal -> {
+                binding.homeScore.text = game.homeTeamScore.toString()
+                binding.awayScore.text = game.awayTeamScore.toString()
+                binding.gameStatus.text = resources.getString(R.string.game_final)
+            }
+            game.inProgress -> {
+                binding.homeScore.text = game.homeTeamScore.toString()
+                binding.awayScore.text = game.awayTeamScore.toString()
+                binding.gameStatus.text = resources.getString(R.string.in_progress)
+            }
+            else -> {
+                binding.homeScore.text = game.homeTeamRecord
+                binding.awayScore.text = game.awayTeamRecord
+                binding.gameStatus.text = resources.getString(R.string.game_number, position + 1)
+            }
         }
 
         if (isUsersSchedule) {
-            viewHolder.simToGame.setOnClickListener { presenter.simulateToGame(game.gameId) }
-            viewHolder.simGame.setOnClickListener { presenter.simulateGame(game.gameId) }
+            binding.simToGame.setOnClickListener { presenter.simulateToGame(game.gameId) }
+            binding.simGame.setOnClickListener { presenter.simulateGame(game.gameId) }
             if (!game.isFinal) {
-                viewHolder.itemView.setOnClickListener {
-                    val vis = viewHolder.simGame.visibility
+                binding.root.setOnClickListener {
+                    val vis = binding.simGame.visibility
                     if (vis == View.VISIBLE) {
-                        viewHolder.simGame.visibility = View.GONE
-                        viewHolder.simToGame.visibility = View.GONE
+                        binding.simGame.visibility = View.GONE
+                        binding.simToGame.visibility = View.GONE
                     } else {
-                        viewHolder.simGame.visibility = View.VISIBLE
-                        viewHolder.simToGame.visibility = View.VISIBLE
+                        binding.simGame.visibility = View.VISIBLE
+                        binding.simToGame.visibility = View.VISIBLE
                     }
                 }
             } else {
-                viewHolder.simGame.visibility = View.GONE
-                viewHolder.simToGame.visibility = View.GONE
+                binding.simGame.visibility = View.GONE
+                binding.simToGame.visibility = View.GONE
             }
         }
     }
